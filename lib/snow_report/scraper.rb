ad9638365bee435066393a324e4b2f48e4e9aa6b
thurman024 <33437392+thurman024@users.noreply.github.com>
2018-02-06 14:51:58 -0600

    scraper functionality corrected

--- lib/snow_report/scraper.rb
+++ lib/snow_report/scraper.rb
@@ -7,18 +7,21 @@ class SnowReport::Scraper
     doc = Nokogiri::HTML(html)
     mountains = []
     table = doc.css(".resortList tr")
-    table.each do |row|
-      mountain_hash = {
-        :name => row.css(".name").text
-        :state => name = row.css(".rRegion").text
-        :snowfall => row.css(".rLeft.b b")[1].text
-        :base_depth => row.css(".rMid.c b").text
-        :link => row.css(".rMid.c a").attribute("href").value #(needs full web link)
-        :runs_open => row.css("td.rMid")[3].text
-      }
-      mountains << mountain_hash
+    table.each_with_index do |row, i|
+      if i > 1 && i < table.size - 3
+          # binding.pry
+        mountain_hash = {
+          :name => row.css(".name").text,
+          :state => row.css(".rRegion").text,
+          :snowfall => row.css(".rLeft.b b")[1].text,
+          :base_depth => row.css(".rMid.c b").text,
+          :link => row.css(".rMid.c a").attribute("href").value, #(needs full web link)
+          :runs_open => row.css("td.rMid")[3].text
+        }
+        mountains << mountain_hash
+      end
     end
-    
+
     binding.pry
   end
 end
